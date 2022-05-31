04aed2b29aaebad3adc1d23c1c023df5057cee77
andywang505 <andywang890505@gmail.com>
2022-05-31 23:34:06 +0800

    20220531-update final version

--- js/all.js
+++ js/all.js
@@ -1,6 +1,35 @@
 $(document).ready(function(){
+    //header
     $('.shownav').on('click', function(e){
         e.preventDefault();
         $('body').toggleClass('nav_show');
     })
+    //gotop
+    $(".gotop a").on('click',function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: 0
+        }, 50);
+    });
+    //feature
+    $("#featureLink").on('click',function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $(".feature").offset().top
+        }, 50);
+    });
+    //chef
+    $("#chefLink").on('click',function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $(".todaychef_wrap").offset().top
+        }, 50);
+    });
+    //map
+    $("#mapLink").on('click',function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $(".booking_wrap").offset().top
+        }, 50);
+    });
 })
